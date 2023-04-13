2d3424bc9f9ed622934080780495a9286ded08a5
Atulsoam562003 <atulsoam5@gmail.com>
2023-04-13 22:19:33 +0530

    I think it's done

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ app.get('/sell' , (req , res) => {
   res.sendFile(__dirname + '/public/sell.html');
 });
 
+
 app.post('/sell' , (req ,res) => {
   let address = req.body.address;
   let bedRooms = req.body.bedrooms;
@@ -40,7 +41,7 @@ app.post('/sell' , (req ,res) => {
   let price  = req.body.price;
   let rent = 0;
 
-   let sql = "INSERT INTO property (p_area , address , bedrooms , bathrooms , city , state , zipcode , price ,on_rent_or_sold) VALUES ?";
+  let sql = "INSERT INTO property (p_area , address , bedrooms , bathrooms , city , state , zipcode , price ,on_rent_or_sold) VALUES ?";
 
   const values = [ [`${propertyArea}` , `${address}` , `${bedRooms}` , `${bathrooms}` , `${city}` , `${state}` , `${zipCode}` , `${price}` , `${rent}`] ]
 
@@ -61,20 +62,20 @@ app.post('/buy', (req, res) => {
   let state = req.body.state;
   let zip = req.body.zip;
 
-    con.query("SELECT * FROM property", (err, result, fields) => {
-      if (err) throw err;
-      // console.log(res);
-      result.forEach((property) => {
+  con.query("SELECT * FROM property", (err, result, fields) => {
+    if (err) throw err;
+    // console.log(res);
+    result.forEach((property) => {
         // console.log(property.city , " " , city);
         if (property.city === city && property.state === state)
-          finalProps.push(property.p_id)
-        });
+        finalProps.push(property.p_id)
+      });
         res.redirect('/property');
       });
-});
+    });
 
-app.post('/rent' , (req ,res) => {
-  let address = req.body.address;
+    app.post('/rent' , (req ,res) => {
+      let address = req.body.address;
   let bedRooms = req.body.bedrooms;
   let bathrooms = req.body.bathrooms;
   let propertyArea = req.body.propertyArea;
@@ -84,7 +85,7 @@ app.post('/rent' , (req ,res) => {
   let price  = req.body.price;
   let rent = 0;
 
-   let sql = "INSERT INTO property (p_area , address , bedrooms , bathrooms , city , state , zipcode , price ,on_rent_or_sold) VALUES ?";
+  let sql = "INSERT INTO property (p_area , address , bedrooms , bathrooms , city , state , zipcode , price ,on_rent_or_sold) VALUES ?";
 
   const values = [ [`${propertyArea}` , `${address}` , `${bedRooms}` , `${bathrooms}` , `${city}` , `${state}` , `${zipCode}` , `${price}` , `${rent}`] ]
 
@@ -100,12 +101,32 @@ app.get('/success' , (req ,res) => {
   res.sendFile(__dirname + '/public/successf.html');
 });
 
+app.get('/enquiry' , (req , res) => {
+
+  let sql = `SELECT name , Email , phone
+            FROM Agent;`
+
+  con.query(sql , (err , result) => {
+    if(err) throw err;
+    let final = [];
+    result.forEach((element) => {
+      let obj = {
+        name : element.name ,
+        email : element.Email ,
+        phone : element.phone
+      };
+      final.push(obj);
+    });
+  res.render(__dirname + '/public/enquiry.ejs' , {agents : final});
+  });
+});
+
 app.get('/property', (req, res) => {
 
-    let sql = `SELECT property.p_id,
-                      Agent.name AS agent_name,
-                      property.city,
-                      property.state,
+  let sql = `SELECT property.p_id,
+  Agent.name AS agent_name,
+  property.city,
+  property.state,
                       property.p_area,
                       property.price,
                       property.bedrooms ,
@@ -140,7 +161,6 @@ app.get('/property', (req, res) => {
         apartments: final
       })
     });
-  // });
 });
 
 app.listen(3000, (err) => {
